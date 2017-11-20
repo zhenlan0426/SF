98c0ae1616e3931bf7259784aaf87597c0d994c9
will <zhenlanwang0426@gmail.com>
2017-11-19 20:42:38 -0500

    Add files via upload

--- functions.py
+++ functions.py
@@ -7,6 +7,7 @@ Created on Sat Nov 11 10:12:15 2017
 """
 import numpy as np
 import pandas as pd
+import cPickle
 
 def isConsecutive(seq):
     # if non-consecutive, need to re-map to consecutive number starting from 1
@@ -42,7 +43,8 @@ def mergeFillCastsss(df0,dfs,keys):
         df0 = mergeFillCast(df0,df,key)
     return df0
 
-def createDataMain(MLP):
+
+def createDataMain(isTrain):
     types = {'id': 'int32',
          'item_nbr': 'int32',
          'store_nbr': 'int8',
@@ -55,8 +57,8 @@ def createDataMain(MLP):
     train.loc[train.unit_sales<0,'unit_sales'] = .0 # clip negative sales to zero
     val = train[train.date >= '2017-07-31']
     train = train[train.date < '2017-07-31']
-    item_uniq = pd.unique(train.item_nbr)
-    item_dict = {item:i+1 for i,item in enumerate(item_uniq)}
+    with open('item_dict_train.pickle' if isTrain else 'item_dict_final.pickle', "rb") as input_file:
+        item_dict = cPickle.load(input_file)
     iter_mapping = lambda x: item_dict[x] if x in item_dict else 0
     
     items = pd.read_csv('items.csv')
@@ -77,8 +79,7 @@ def createDataMain(MLP):
         SI_train['item_nbr'] = SI_train.item_nbr.map(iter_mapping)
         SI_train['amin'] = pd.to_datetime(SI_train['amin'])
         SI_train['amax'] = pd.to_datetime(SI_train['amax'])
-        if MLP:
-            SI_train['countDays'] = (SI_train['amax'] - SI_train['amin']).dt.days + 1
+        SI_train['countDays'] = (SI_train['amax'] - SI_train['amin']).dt.days + 1
             
         return SI_train[['store_nbr',
                          'item_nbr',
@@ -100,7 +101,7 @@ def createDataMain(MLP):
     valRNN = CreateData(val)
     return trainRNN,valRNN
 
-def createTestDataMain(isTrain=True):
+def createTestDataMain(isTrain):
     types = {'id': 'int32',
          'item_nbr': 'int32',
          'store_nbr': 'int8',
@@ -116,8 +117,8 @@ def createTestDataMain(isTrain=True):
     train.loc[train.unit_sales<0,'unit_sales'] = .0 # clip negative sales to zero
     if isTrain:
         train = train[train.date < '2017-07-31']
-    item_uniq = pd.unique(train.item_nbr)
-    item_dict = {item:i+1 for i,item in enumerate(item_uniq)}
+    with open('item_dict_train.pickle' if isTrain else 'item_dict_final.pickle', "rb") as input_file:
+        item_dict = cPickle.load(input_file)
     iter_mapping = lambda x: item_dict[x] if x in item_dict else 0
     
     items = pd.read_csv('items.csv')
@@ -172,130 +173,81 @@ def createTestDataMain(isTrain=True):
     return test_SI.reset_index(drop=True),test_newItem.reset_index(drop=True)
 
 
-def RNN_generator(data,batchSize,seqSize,df,key,discreteList,shuffle=True,downSample=1):
-    # return bool (if init should be reset) 
-    # and a list [y (B,T),weight (B,T),Xcontinue of shape (B,T,2)] + [Xdiscrete] of shape (B,T) + [X] of shape (B,)
-    # finetune by use store-item pair in testset only
-    if shuffle:
-        data = data.sample(frac=1).reset_index(drop=True)
-    n = data.shape[0]
 
-    for from_ in range(0,n-batchSize,batchSize):
-        X = list(data.loc[from_:from_+batchSize-1,'store_nbr':'cluster'].values.astype(np.int32).T)
-        weight = np.ones((batchSize,1),dtype=np.float32)
-        weight[data.loc[from_:from_+batchSize-1,'perishable']==1] = 1.25
-        for j,(y,w,Xdiscrete,Xcontinue) in enumerate(timeGenerator(\
-                           data.loc[from_:from_+batchSize-1,'date':'amax'],seqSize,downSample,df,key,discreteList)):
-            yield j==0, [y,weight*w,Xcontinue] + Xdiscrete + X
-            
-def timeGenerator(data,seqSize,downSample,df,key,discreteList):
-    # df is time related variables like holiday and seasonality
-    # returns y, weight, X_discrete, X_continue
-    n = data.shape[0]
-    data['curr'] = data.amin 
-    sparse = pd.concat([data.apply(lambda x: pd.Series(x.date,name='date'),axis=1)\
-                                    .stack().reset_index().drop(['level_1'],1),\
-                        data.apply(lambda x: pd.Series(x.unit_sales),axis=1)\
-                                    .stack().reset_index().drop(['level_0','level_1'],1),\
-                        data.apply(lambda x: pd.Series(x.onpromotion),axis=1)\
-                                    .stack().reset_index().drop(['level_0','level_1'],1)],1)
-    sparse.columns = ['level','date','sales','onpromotion']
-    while np.all(data.curr + pd.DateOffset(seqSize) <= data['amax']):
-        dense = data.apply(lambda x:pd.Series(pd.date_range(x.curr,periods=seqSize+1)),axis=1)\
+
+
+def pd2np(data,batchSize,seqSize,df,key,discreteList):
+    
+    def RNN_generator2(data,batchSize,seqSize,df,key,discreteList):
+        n = data.shape[0]
+        for from_ in range(0,n,batchSize):
+            for y,Xdiscrete,Xcontinue in timeGenerator2(data.loc[from_:from_+batchSize-1,'date':'amax'],\
+                                                          seqSize,df,key,discreteList):
+                yield y,Xcontinue, Xdiscrete
+                
+    def timeGenerator2(data,seqSize,df,key,discreteList):
+        # df is time related variables like holiday and seasonality
+        # returns y, X_discrete, X_continue
+        n = data.shape[0]
+        sparse = pd.concat([data.apply(lambda x: pd.Series(x.date,name='date'),axis=1)\
+                                        .stack().reset_index().drop(['level_1'],1),\
+                            data.apply(lambda x: pd.Series(x.unit_sales),axis=1)\
+                                        .stack().reset_index().drop(['level_0','level_1'],1),\
+                            data.apply(lambda x: pd.Series(x.onpromotion),axis=1)\
+                                        .stack().reset_index().drop(['level_0','level_1'],1)],1)
+        sparse.columns = ['level','date','sales','onpromotion']
+    
+        dense = data.apply(lambda x:pd.Series(pd.date_range(x.amin,periods=seqSize)),axis=1)\
                             .stack().reset_index().drop(['level_1'],1)
         dense.columns = ['level','date']
         dense = pd.merge(pd.merge(dense, df[np.random.randint(10)], how='left', on=key),
                         sparse,how='left',on=['level','date']).fillna(0)
-        dense_continue = dense[['dcoilwtico','sales']].values.astype(np.float32)\
-                                  .reshape((n,seqSize+1,2))[:,:seqSize,:]
-        dense_discrete = list(np.moveaxis(dense[discreteList].values.astype(np.int32)\
-                                                .reshape((n,seqSize+1,len(discreteList)))[:,1:,:]\
-                                          ,2,0))
-        y = dense['sales'].values.astype(np.float32).reshape((n,seqSize+1))[:,1:]
-        weight = np.ones_like(y,dtype=np.float32)
-        weight[y==0] = downSample
-        yield y, weight, dense_discrete, dense_continue
-        data.curr = data.curr + pd.DateOffset(seqSize)
-        
-def MLP_generator(data,batchSize,seqSize,df,key,discreteList,shuffle=True,downSample=1):
-    # return bool (if init should be reset) 
-    # and a list [y (B,T),weight (B,T),Xcontinue of shape (B,T,2)] + [Xdiscrete] of shape (B,T) + [X] of shape (B,)
-    # finetune by use store-item pair in testset only
-    if shuffle:
-        data = data.sample(frac=1).reset_index(drop=True)
+        dense_continue = dense['dcoilwtico'].values.astype(np.float32).reshape((n,seqSize))
+        dense_discrete = dense[discreteList].values.astype(np.int32)\
+                            .reshape((n,seqSize,len(discreteList)))
+        y = dense['sales'].values.astype(np.float32).reshape((n,seqSize))
+    
+        yield y, dense_discrete, dense_continue
+    
     n = data.shape[0]
+    Y,Con,Dis = [],[],[]
+    for y,con,dis in RNN_generator2(data,batchSize,seqSize,df,key,discreteList):
+        Y.append(y)
+        Con.append(con)
+        Dis.append(dis)       
+    weight = np.ones((n,1),dtype=np.float32)
+    weight[data['perishable']==1] = 1.25
+    return np.concatenate(Y), weight,np.concatenate(Con),\
+            np.concatenate(Dis),data.loc[:,'store_nbr':'cluster'].values.astype(np.int32),\
+            data['countDays'].values.astype(np.int32)
 
-    for from_ in range(0,n-batchSize,batchSize):
-        X = list(data.loc[from_:from_+batchSize-1,'store_nbr':'cluster'].values.astype(np.int32).T)
-        weight = np.ones((batchSize,1),dtype=np.float32)
-        weight[data.loc[from_:from_+batchSize-1,'perishable']==1] = 1.25
-        for y,w,Xdiscrete,Xcontinue in MLP_timeGenerator(\
-                           data.loc[from_:from_+batchSize-1,'date':'amax'],seqSize,downSample,df,key,discreteList):
-            yield [y,weight*w,Xcontinue] + Xdiscrete + X
-            
-def MLP_timeGenerator(data,seqSize,downSample,df,key,discreteList):
-    # df is time related variables like holiday and seasonality
-    # returns y, weight, X_discrete, X_continue
-    n = data.shape[0]
-    sparse = pd.concat([data.apply(lambda x: pd.Series(x.date,name='date'),axis=1)\
-                                    .stack().reset_index().drop(['level_1'],1),\
-                        data.apply(lambda x: pd.Series(x.unit_sales),axis=1)\
-                                    .stack().reset_index().drop(['level_0','level_1'],1),\
-                        data.apply(lambda x: pd.Series(x.onpromotion),axis=1)\
-                                    .stack().reset_index().drop(['level_0','level_1'],1)],1)
-    sparse.columns = ['level','date','sales','onpromotion']
 
-    dense = data.apply(lambda x:pd.Series(pd.date_range(x.amin,x.amax))\
-                        .iloc[np.random.choice(x.countDays,seqSize,False)],axis=1)\
-                        .stack().reset_index().drop(['level_1'],1)
-    dense.columns = ['level','date']
-    dense = pd.merge(pd.merge(dense, df[np.random.randint(10)], how='left', on=key),
-                    sparse,how='left',on=['level','date']).fillna(0)
-    dense_continue = dense['dcoilwtico'].values.astype(np.float32)\
-                              .reshape((n,seqSize,1))
-    dense_discrete = list(np.moveaxis(dense[discreteList].values.astype(np.int32)\
-                                            .reshape((n,seqSize,len(discreteList)))\
-                                      ,2,0))
-    y = dense['sales'].values.astype(np.float32).reshape((n,seqSize))
-    weight = np.ones_like(y,dtype=np.float32)
-    weight[y==0] = downSample
-    yield y, weight, dense_discrete, dense_continue
 
-def MLP_Test_generator(data,batchSize,seqSize,df,key,discreteList):
-    # return bool (if init should be reset) 
-    # and a list [id (B,T),Xcontinue of shape (B,T,1)] + [Xdiscrete] of shape (B,T) + [X] of shape (B,)
-    # finetune by use store-item pair in testset only
-    n = data.shape[0]
-
-    for from_ in range(0,n,batchSize):
-        X = list(data.loc[from_:from_+batchSize-1,'store_nbr':'cluster'].values.astype(np.int32).T)
-
-        for id_,Xdiscrete,Xcontinue in MLP_Test_timeGenerator(\
-                           data.loc[from_:from_+batchSize-1,'id':'amax'],seqSize,df,key,discreteList):
-            yield [id_,Xcontinue] + Xdiscrete + X
-            
-def MLP_Test_timeGenerator(data,seqSize,df,key,discreteList):
-    # df is time related variables like holiday and seasonality
-    # returns id_, X_discrete, X_continue
-    n = data.shape[0]
-    sparse = pd.concat([data.apply(lambda x: pd.Series(x.id),axis=1)\
-                                    .stack().reset_index().drop(['level_1'],1),\
-                        data.apply(lambda x: pd.Series(x.onpromotion),axis=1)\
-                                    .stack().reset_index().drop(['level_0','level_1'],1)],1)
-    sparse.columns = ['level','id','onpromotion']
-
-    dense = data.apply(lambda x:pd.Series(pd.date_range(x.amin,x.amax))\
-                        .iloc[np.random.choice(x.countDays,seqSize,False)],axis=1)\
-                        .stack().reset_index().drop(['level_1'],1)
-    dense.columns = ['level','date']
-    dense = pd.merge(pd.merge(dense, df[np.random.randint(10)], how='left', on=key),
-                    sparse,how='left',on=['level','date']).fillna(0)
-    dense_continue = dense['dcoilwtico'].values.astype(np.float32)\
-                              .reshape((n,seqSize,1))
-    dense_discrete = list(np.moveaxis(dense[discreteList].values.astype(np.int32)\
-                                            .reshape((n,seqSize,len(discreteList)))\
-                                      ,2,0))
-    y = dense['sales'].values.astype(np.float32).reshape((n,seqSize))
-    weight = np.ones_like(y,dtype=np.float32)
-    weight[y==0] = downSample
-    yield y, weight, dense_discrete, dense_continue
+#def RNN_generator(y_np, weight_np,Con_np,Dis_np,X_np,Count_np,\
+#                  batchSize,seqSize,shuffle=True,downSample=1):
+#    # yield a list [y,Weight,X_continuous] + Xt + X
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
