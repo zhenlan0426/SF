025f37b85315eaf9242c5ca8f790e62832b9494e
will <zhenlanwang0426@gmail.com>
2017-12-06 19:26:04 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,91 @@
 # TODO: SI start at Store start date vs SI start date
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+Train loss:0.892164247098,Val Loss:1.52009364216
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+Train loss:0.654324108227,Val Loss:1.27895200318
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+Train loss:0.584741685997,Val Loss:1.1458360961
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+Train loss:0.76468003454,Val Loss:1.07932966074
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+Train loss:0.692669850179,Val Loss:1.03746977613
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+Train loss:0.70496979123,Val Loss:1.00797572034
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+Train loss:0.830863437933,Val Loss:0.988632531742
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+Train loss:0.98472004448,Val Loss:0.970990666069
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+Train loss:0.580152240089,Val Loss:0.959448956301
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+Train loss:0.693012832798,Val Loss:0.947345156133
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+Train loss:0.870238032811,Val Loss:0.937034491361
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+Train loss:0.80733389746,Val Loss:0.927681529899
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+Train loss:0.612991914407,Val Loss:0.919241911061
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+Train loss:0.7915078838,Val Loss:0.91110062619
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+Train loss:0.915603727971,Val Loss:0.903920896736
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+Train loss:0.536337624926,Val Loss:0.897050998446
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+Train loss:0.47972343255,Val Loss:0.891192807346
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+Train loss:0.797278241242,Val Loss:0.885700801063
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+Train loss:0.696156403266,Val Loss:0.880902680477
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+Train loss:0.571866233075,Val Loss:0.877048614673
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+Train loss:0.582315119444,Val Loss:0.873835094767
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+Train loss:0.864367181693,Val Loss:0.87029202093
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+Train loss:0.782643144767,Val Loss:0.867611102869
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+Train loss:0.562042470596,Val Loss:0.865135824477
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+Train loss:0.443911270498,Val Loss:0.86258852684
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+Train loss:0.705587461438,Val Loss:0.860363409197
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+Train loss:0.550457470224,Val Loss:0.858455269834
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+Train loss:0.624601047267,Val Loss:0.856905005867
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+Train loss:0.637396027096,Val Loss:0.855589383473
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
+Train loss:0.718809293291,Val Loss:0.853829751597
+INFO:tensorflow:Restoring parameters from RNN_fillin_01
