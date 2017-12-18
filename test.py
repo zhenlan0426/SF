24a1b9dfd4d84547348d0f074848c078f71ef860
will <zhenlanwang0426@gmail.com>
2017-12-18 16:41:56 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,91 +1,64 @@
 # TODO: SI start at Store start date vs SI start date
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-Train loss:0.892164247098,Val Loss:1.52009364216
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-Train loss:0.654324108227,Val Loss:1.27895200318
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-Train loss:0.584741685997,Val Loss:1.1458360961
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-Train loss:0.76468003454,Val Loss:1.07932966074
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-Train loss:0.692669850179,Val Loss:1.03746977613
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-Train loss:0.70496979123,Val Loss:1.00797572034
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-Train loss:0.830863437933,Val Loss:0.988632531742
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-Train loss:0.98472004448,Val Loss:0.970990666069
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-Train loss:0.580152240089,Val Loss:0.959448956301
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-Train loss:0.693012832798,Val Loss:0.947345156133
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-Train loss:0.870238032811,Val Loss:0.937034491361
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-Train loss:0.80733389746,Val Loss:0.927681529899
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-Train loss:0.612991914407,Val Loss:0.919241911061
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-Train loss:0.7915078838,Val Loss:0.91110062619
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-Train loss:0.915603727971,Val Loss:0.903920896736
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-Train loss:0.536337624926,Val Loss:0.897050998446
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-Train loss:0.47972343255,Val Loss:0.891192807346
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-Train loss:0.797278241242,Val Loss:0.885700801063
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-Train loss:0.696156403266,Val Loss:0.880902680477
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-Train loss:0.571866233075,Val Loss:0.877048614673
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-Train loss:0.582315119444,Val Loss:0.873835094767
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-Train loss:0.864367181693,Val Loss:0.87029202093
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-Train loss:0.782643144767,Val Loss:0.867611102869
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-Train loss:0.562042470596,Val Loss:0.865135824477
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-Train loss:0.443911270498,Val Loss:0.86258852684
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-Train loss:0.705587461438,Val Loss:0.860363409197
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-Train loss:0.550457470224,Val Loss:0.858455269834
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-Train loss:0.624601047267,Val Loss:0.856905005867
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-Train loss:0.637396027096,Val Loss:0.855589383473
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
-Train loss:0.718809293291,Val Loss:0.853829751597
-INFO:tensorflow:Restoring parameters from RNN_fillin_01
+# fixedPara = [(cardinalitys_X,v1),cardinalitys_T,dimentions_X,dimentions_T,dX,d] a list of tuple
+import os
+def hyperSearch_epoch(y_np, weight_np,Con_np,Dis_np,X_np,Count_np,\
+                      y_np_val, weight_np_val,Con_np_val,Dis_np_val,X_np_val,Count_np_val,\
+                      paras,fixedPara,learningRate,downsample,index,SavePath,check_points=[15,20,25,30]):
+    # paras should be a list of tuple [(paraName,paraValue)...]
+    inputs,train_op,cost,saver,yhat,state = createGraphRNN2(**dict(paras+fixedPara))
+    sess = tf.InteractiveSession()
+    sess.run(tf.global_variables_initializer())
+    
+    model_name = '-'
+    model_name.join([name+':'+str(value) for name,value in paras+[('downsample',downsample)]])
+    best_name = ''
+    best_loss = 100
+    init_state = tuple([tf.contrib.rnn.LSTMStateTuple(np.zeros((model_para['batch_size'],model_para['d']),\
+                                                                   dtype=np.float32),\
+                                                      np.zeros((model_para['batch_size'],model_para['d']),\
+                                                                   dtype=np.float32))\
+                                                    for i in range(model_para['n_layers'])]) \
+                 if model_para['cell_type'] == 'NormLSTM' else \
+                 tuple([np.zeros((model_para['batch_size'],model_para['d']),dtype=np.float32) \
+                        for i in range(model_para['n_layers'])]) 
+    for i in range(1,max(check_points)+1):
+        for j,X_nps in enumerate(RNN_generator(y_np, weight_np,Con_np,Dis_np,X_np,Count_np,\
+                                  model_para['batch_size'],model_para['seq_len'],10000,downSample=downsample)):
+            _,init_state = sess.run([train_op,state],\
+                                 dict(zip(inputs,X_nps+[learningRate,init_state])))
+        
+        if i in check_points:
+            fullPath = SavePath+'/'+model_name+'-epoch:'+str(i)
+            saver.save(sess,fullPath)
+            inputs,train_op,cost,saver,yhat,state = createGraphRNN2(None,1,cardinalitys_X,cardinalitys_T,\
+                                                    dimentions_X,dimentions_T,dX,d,keep_prob,n_layers,\
+                                                    grad_clip,cell_type,optimizer,actFun)
+            sess = tf.InteractiveSession()
+            sess.run(tf.global_variables_initializer())
+            saver.restore(sess,fullPath)
+            if model_para['cell_type'] == 'NormLSTM':
+                init_tot_list = init_state_update_LSTM(sess,inputs,state,batch_size*10,d,n_layers,\
+                              y_np[index],Con_np[index],X_np[index],Count_np[index],\
+                              [dis[index] for dis in Dis_np])
+                y_val_hat = RNN_forecast_Repeat_LSTM(10,sess,inputs,state,yhat,batch_size*10,n_layers,\
+                                                np.expand_dims(y_np[index,Count_np[index]-1],-1),\
+                                                Con_np_val,X_np_val,Dis_np_val,init_tot_list)
+            else:    
+                init_tot_list = init_state_update(sess,inputs,state,batch_size*10,d,n_layers,\
+                                      y_np[index],Con_np[index],X_np[index],Count_np[index],\
+                                      [dis[index] for dis in Dis_np])
+                y_val_hat = RNN_forecast_Repeat(10,sess,inputs,state,yhat,batch_size*10,n_layers,\
+                                                np.expand_dims(y_np[index,Count_np[index]-1],-1),\
+                                                Con_np_val,X_np_val,Dis_np_val,init_tot_list)
+            loss = loss_func(weight_np_val[:,np.newaxis],y_val_hat,y_np_val)                
+            if loss < best_loss:
+                if best_name != '':
+                    os.remove(best_name)
+                best_name = fullPath
+                best_loss = loss
+            else:
+                os.remove(fullPath)
+            print "loss:{} ,epoch:{} ,batch_size:{} ,seq_len:{} ,keep_prob:{} ,n_layers:{} ,grad_clip:{} ,cell_type:{} ,downsample:{} ,optimizer:{} ,actFun:{} \n"\
+              .format(loss,i,batch_size,seq_len,keep_prob,n_layers,grad_clip,cell_type,downsample,optimizer,actFun)
+        
+        return best_name
